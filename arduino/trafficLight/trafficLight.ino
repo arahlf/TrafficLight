2803287e40d2383d6f1c42aada8ed41f0d599678
arahlf <arahlf@gmail.com>
2015-02-15 11:21:33 -0600

    Updated the flashing logic to not use delay so that the program is more responsive.

--- arduino/trafficLight/trafficLight.ino
+++ arduino/trafficLight/trafficLight.ino
@@ -2,10 +2,15 @@ const int RED_PIN = 4;
 const int YELLOW_PIN = 3;
 const int GREEN_PIN = 2;
 
+const int VOID_PIN= -1;
+
 const int pins[] = { RED_PIN, YELLOW_PIN, GREEN_PIN };
 const int pinCount = sizeof(pins) / sizeof(int);
 
-int flashingPin = -1;
+int flashingPin = VOID_PIN;
+boolean flashingHigh;
+const unsigned long flashDuration = 750;
+unsigned long flashStartTime;
 
 void setup() {
     Serial.begin(9600);
@@ -18,12 +23,13 @@ void setup() {
 
 void loop() {
   
-    if (flashingPin != -1) {
-        // TODO use millis() to compare time instead of delays so that it's more responsive
-        digitalWrite(flashingPin, HIGH);
-        delay(750);
-        digitalWrite(flashingPin, LOW);
-        delay(750);
+    if (flashingPin != VOID_PIN) {
+        if ((millis() - flashStartTime) >= flashDuration) {
+            flashingHigh = !flashingHigh;
+            flashStartTime = millis();
+            
+            digitalWrite(flashingPin, flashingHigh);
+        }
     }
 
     if (Serial.available()) {
@@ -34,37 +40,41 @@ void loop() {
 
         if (command == "light red") {
             lightPinExclusive(RED_PIN);
-            flashingPin = -1;
         }
         else if (command == "light yellow") {
             lightPinExclusive(YELLOW_PIN);
-            flashingPin = -1;
         }
         else if (command == "light green") {
             lightPinExclusive(GREEN_PIN);
-            flashingPin = -1;
         }
         else if (command == "flash red") {
-            lightPinExclusive(-1);
-            flashingPin = RED_PIN;
+            flashPinExclusive(RED_PIN);
         }
         else if (command == "flash yellow") {
-            lightPinExclusive(-1);
-            flashingPin = YELLOW_PIN;
+            flashPinExclusive(YELLOW_PIN);
         }
         else if (command == "flash green") {
-            lightPinExclusive(-1);
-            flashingPin = GREEN_PIN;
+            flashPinExclusive(GREEN_PIN);
         }
         else if (command == "lights off") {
-            lightPinExclusive(-1);
-            flashingPin = -1;
+            lightPinExclusive(VOID_PIN);
         }
     }
 }
 
 void lightPinExclusive(int pin) {
+    flashingPin = VOID_PIN;
+    
     for (int i = 0; i < pinCount; i++) {
         digitalWrite(pins[i], pins[i] == pin ? HIGH : LOW);
     }
 }
+
+void flashPinExclusive(int pin) {
+    lightPinExclusive(pin);
+    
+    flashingPin = pin;
+    flashingHigh = true;
+    flashStartTime = millis();
+}
+
